088ffb64906a8bd8eea02320396af9d849dd1680
lamnaourm <mohammed.lamnaour@gmail.com>
2023-09-27 14:48:12 +0100

    UserCard component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import './App.css';
 
+const utilisateur = {
+  nom: "Doe",
+  prénom: "John",
+  email: "john.doe@example.com",
+  ImageUrl: "https://icones.pro/wp-content/uploads/2021/03/avatar-de-personne-icone-homme.png",
+ };
+ 
 function App() {
   return (
     <div className="container">
